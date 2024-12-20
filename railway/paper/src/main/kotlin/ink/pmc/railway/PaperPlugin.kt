17d68fae7ff760cb122748555ebfd2aeac018d16
nostalfinals <yuu8583@proton.me>
2024-12-20 17:16:34 +0800

    Init

---
+++ railway/paper/src/main/kotlin/ink/pmc/railway/PaperPlugin.kt
@@ -0,0 +1,25 @@
+package ink.pmc.railway
+
+import com.github.shynixn.mccoroutine.bukkit.SuspendingJavaPlugin
+import ink.pmc.framework.inject.startKoinIfNotPresent
+import org.bukkit.plugin.Plugin
+import org.koin.dsl.module
+
+internal lateinit var plugin: Plugin
+
+class PaperPlugin : SuspendingJavaPlugin() {
+    private val bukkitModule = module {
+
+    }
+
+    override fun onEnable() {
+        plugin = this
+        startKoinIfNotPresent {
+            modules(bukkitModule)
+        }
+    }
+
+    override fun onDisable() {
+
+    }
+}

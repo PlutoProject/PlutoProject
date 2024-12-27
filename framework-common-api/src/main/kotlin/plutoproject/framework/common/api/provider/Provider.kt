990837e01827301df9ff2b4f20de3dc8438c7f3e
nostalfinals <yuu8583@proton.me>
2024-12-27 20:38:54 +0800

    feat: 迁移 Provider API

--- framework-common-api/src/main/kotlin/plutoproject/framework/common/api/provider/Provider.kt
+++ framework-common-api/src/main/kotlin/plutoproject/framework/common/api/provider/Provider.kt
@@ -1,15 +1,11 @@
-package ink.pmc.framework.provider
+package plutoproject.framework.common.api.provider
 
 import com.maxmind.geoip2.DatabaseReader
 import com.mongodb.kotlin.client.coroutine.MongoClient
 import com.mongodb.kotlin.client.coroutine.MongoCollection
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
-import ink.pmc.framework.inject.inlinedGet
+import plutoproject.framework.common.util.inject.inlinedGet
 import java.io.Closeable
-
-inline fun <reified T : Any> Provider.getCollection(name: String): MongoCollection<T> {
-    return defaultMongoDatabase.getCollection(name)
-}
 
 interface Provider : Closeable {
     companion object : Provider by inlinedGet()
@@ -17,4 +13,8 @@ interface Provider : Closeable {
     val mongoClient: MongoClient
     val defaultMongoDatabase: MongoDatabase
     val geoIpDatabase: DatabaseReader
+}
+
+inline fun <reified T : Any> Provider.getCollection(name: String): MongoCollection<T> {
+    return defaultMongoDatabase.getCollection(name)
 }

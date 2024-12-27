8f793d0ee83a4740bef22f0d92ca54776bd0553a
nostalfinals <yuu8583@proton.me>
2024-12-27 22:01:15 +0800

    refactor: 开始迁移 Framework Paper

--- framework-common-api/src/main/kotlin/plutoproject/framework/common/util/Platform.kt
+++ framework-common-api/src/main/kotlin/plutoproject/framework/common/util/Platform.kt
@@ -6,20 +6,18 @@ lateinit var serverThread: Thread
 lateinit var pluginDataFolder: File
 lateinit var featureDataFolder: File
 lateinit var frameworkDataFolder: File
-lateinit var featureResourcePrefix: String
-lateinit var frameworkResourcePrefix: String
+
+fun getFrameworkModuleDataFolder(id: String): File {
+    val file = frameworkDataFolder.resolve(id)
+    file.parentFile.mkdirs()
+    return file
+}
 
 fun File.initPluginDataFolder() {
     pluginDataFolder = this
-    if (!(pluginDataFolder.exists())) {
-        pluginDataFolder.mkdirs()
-    }
-    featureDataFolder = File(pluginDataFolder, "feature${File.separator}")
-    frameworkDataFolder = File(pluginDataFolder, "framework${File.separator}")
-    if (!(featureDataFolder.exists())) {
-        featureDataFolder.mkdirs()
-    }
-    if (!(frameworkDataFolder.exists())) {
-        featureDataFolder.mkdirs()
-    }
+    pluginDataFolder.mkdirs()
+    featureDataFolder = pluginDataFolder.resolve("feature${File.separator}")
+    frameworkDataFolder = pluginDataFolder.resolve("framework${File.separator}")
+    featureDataFolder.mkdirs()
+    featureDataFolder.mkdirs()
 }

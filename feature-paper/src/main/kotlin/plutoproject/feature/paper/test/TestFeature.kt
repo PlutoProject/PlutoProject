c8eb10e24949f45df301938541914a7b102d79fd
nostalfinals <yuu8583@proton.me>
2024-12-30 01:43:18 +0800

    feat: Feature 注解

--- feature-paper/src/main/kotlin/plutoproject/feature/paper/test/TestFeature.kt
+++ feature-paper/src/main/kotlin/plutoproject/feature/paper/test/TestFeature.kt
@@ -1,7 +1,20 @@
 package plutoproject.feature.paper.test
 
+import plutoproject.framework.common.api.feature.annotation.Dependency
+import plutoproject.framework.common.api.feature.annotation.Feature
+import plutoproject.framework.common.api.feature.annotation.Load
+import plutoproject.framework.common.api.feature.annotation.Platform
 import plutoproject.framework.paper.api.feature.PaperFeature
 
+@Feature(
+    id = "test_feature",
+    platform = Platform.PAPER,
+    dependencies = [
+        Dependency(id = "test_dependency_feature", load = Load.AFTER, required = false),
+        Dependency(id = "player_teleport", load = Load.AFTER, required = true),
+        Dependency(id = "random_teleport", load = Load.BEFORE, required = true),
+    ]
+)
 class TestFeature : PaperFeature() {
     override fun onLoad() {
     }

eaee52f3c46ae607f9e6d12dae74211fbf983395
nostalfinals <yuu8583@proton.me>
2024-12-31 20:41:41 +0800

    fix: 补上忘记加的 data 关键字

--- framework-common/src/main/kotlin/plutoproject/framework/common/feature/metadata/DependencyMetadata.kt
+++ framework-common/src/main/kotlin/plutoproject/framework/common/feature/metadata/DependencyMetadata.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 import plutoproject.framework.common.api.feature.annotation.Load
 
 @Serializable
-class DependencyMetadata(
+data class DependencyMetadata(
     val id: String,
     val load: Load,
     val required: Boolean

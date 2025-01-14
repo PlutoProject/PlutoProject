a8488d7175e24cc32398b9ad65d636c0d0a5ba75
nostalfinals <yuu8583@proton.me>
2025-01-14 15:03:15 +0800

    chore: 提升版本号到 1.4.0

--- build-logic/src/main/kotlin/plutoproject.base-conventions.gradle.kts
+++ build-logic/src/main/kotlin/plutoproject.base-conventions.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "ink.pmc.plutoproject"
-version = "1.3.0"
+version = "1.4.0"
 
 val dependencyExtension =
     dependencies.extensions.create<PlutoDependencyHandlerExtension>(
